35657c493b1831aca7a9830afa25202a6e01c2e7
oudream <oudream@126.com>
2018-07-13 13:54:24 +0800

    add_custom_command add_custom_target file(GLOB_RECURSE
    add_custom_command add_custom_target file(GLOB_RECURSE

---
+++ hello1/module/sub1/sub11.hpp
@@ -0,0 +1,10 @@
+//
+// Created by oudream on 2018/7/12.
+//
+
+#include <iostream>
+
+int showSub11()
+{
+    std::cout << "hello Sub11" << std::endl;
+}
---
+++ hello1/module/sub1/sub12.hpp
@@ -0,0 +1,10 @@
+//
+// Created by oudream on 2018/7/12.
+//
+
+#include <iostream>
+
+int showSub12()
+{
+    std::cout << "hello Sub12" << std::endl;
+}
